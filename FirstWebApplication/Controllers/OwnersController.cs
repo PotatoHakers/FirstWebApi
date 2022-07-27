bac4bf7cd602e7367015092558004a4a48ff3d41
Gerzha Alex <s.herza@mail.ru>
2022-07-27 10:23:33 +0300

    some changes

---
+++ FirstWebApplication/Controllers/OwnersController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FirstWebApplication.Models;
+
+namespace FirstWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public OwnersController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Owners
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Owner>>> GetOwner()
+        {
+          if (_context.Owner == null)
+          {
+              return NotFound();
+          }
+            return await _context.Owner.ToListAsync();
+        }
+
+        // GET: api/Owners/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Owner>> GetOwner(int id)
+        {
+          if (_context.Owner == null)
+          {
+              return NotFound();
+          }
+            var owner = await _context.Owner.FindAsync(id);
+
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return owner;
+        }
+
+        // PUT: api/Owners/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOwner(int id, Owner owner)
+        {
+            if (id != owner.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(owner).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OwnerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Owners
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Owner>> PostOwner(Owner owner)
+        {
+          if (_context.Owner == null)
+          {
+              return Problem("Entity set 'ApplicationContext.Owner'  is null.");
+          }
+            _context.Owner.Add(owner);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOwner", new { id = owner.Id }, owner);
+        }
+
+        // DELETE: api/Owners/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOwner(int id)
+        {
+            if (_context.Owner == null)
+            {
+                return NotFound();
+            }
+            var owner = await _context.Owner.FindAsync(id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            _context.Owner.Remove(owner);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OwnerExists(int id)
+        {
+            return (_context.Owner?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FirstWebApplication/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstWebApplication.Models
+{
+    [Table("Сars")]
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        
+        public int OwnerId { get; set; }
+        public Owner? Owner { get; set; }
+    }
+}
